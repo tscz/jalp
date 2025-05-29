88162c9b4b7607820e3b1e40d04a3ac292740a1d
tscz <tscz@users.noreply.github.com>
2025-05-29 13:46:25 +0200

    add basic ai support

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,30 @@ dotenv.config();
 
 import { App } from "@slack/bolt";
 
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 // Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
+app.message(async ({ message, say }) => {
     app.logger.info(message)
 
     if (message.subtype === 'bot_message' || message.subtype === "message_changed" || message.subtype === "message_deleted" || message.subtype === "message_replied") return;
 
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: message.text || "error",
+    });
+    app.logger.info(response)
+
     // say() sends a message to the channel where the event was triggered
     await say(`Hey there <@${message.user}>!`);
+    await say(response.text || "error")
 });
 
 (async () => {
